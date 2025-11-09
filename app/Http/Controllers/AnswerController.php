1557e0ba83489321021233f2827930ec1f1f2e70
Claire M <clairemc3@gmail.com>
2025-11-09 20:46:35 +0000

    Remove comments

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -29,8 +29,6 @@ class AnswerController extends Controller
               'value' => $validated['value'] ?? null,
           ]);
 
-       // Here you would typically create or update the answer in the database
-       // For now, just return the validated data
        return response()->json([
            'success' => true,
            'data' => $validated,
